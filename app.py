2ff8060696d3df449c41a4541d7e3fb9d2dcb583
cfp-ecc-eaa6020 <irina.bulat@gmail.com>
2024-05-15 10:07:38 +0000

    fix problem

---
+++ app.py
@@ -0,0 +1,7 @@
+whattodo_list = []
+whattodo_list.append ("Update Cineville subscription")
+whattodo_list.append ("Organize Gyoza workshop")
+whattodo_list.append ("Get sunglasses with prescription lens")
+for todo in whattodo_list: 
+    print (whattodo)
+    
